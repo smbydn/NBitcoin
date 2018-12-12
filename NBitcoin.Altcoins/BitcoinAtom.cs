f384be8ac3b0e9c8bcc48fb30f674c277dc53ac6
smbydn <dmitrii.kaigorodov@yandex.ru>
2018-12-12 15:03:41 +0300

    added CoinbaseMaturity and genesis for regtest

--- NBitcoin.Altcoins/BitcoinAtom.cs
+++ NBitcoin.Altcoins/BitcoinAtom.cs
@@ -79,6 +79,7 @@ namespace NBitcoin.Altcoins
 				MinimumChainWork = new uint256("0x000000000000000000000000000000000000000000f7a10d870760a5efb2aef8"),
 				ConsensusFactory = BitcoinAtomConsensusFactory.Instance,
 				SupportSegwit = true,
+				CoinbaseMaturity = 100
 			})
 			.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { 23 })
 			.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { 10 })
@@ -123,6 +124,7 @@ namespace NBitcoin.Altcoins
 				MinimumChainWork = new uint256("0x00000000000000000000000000000000000000000000003480f4fb0959dfdff3"),
 				ConsensusFactory = BitcoinAtomConsensusFactory.Instance,
 				SupportSegwit = true,
+				CoinbaseMaturity = 100
 			})
 			.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { 111 })
 			.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { 196 })
@@ -167,6 +169,7 @@ namespace NBitcoin.Altcoins
 				MinimumChainWork = new uint256("0x00"),
 				ConsensusFactory = BitcoinAtomConsensusFactory.Instance,
 				SupportSegwit = true,
+				CoinbaseMaturity = 100
 			})
 			.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { 111 })
 			.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { 196 })
@@ -180,7 +183,7 @@ namespace NBitcoin.Altcoins
 			.SetMagic(0xcad71f4a)
 			.SetName("bca-reg")
 			.AddSeeds(new NetworkAddress[0])
-			.SetGenesis("0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4adae5494dffff001d1aa4ae180101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000");
+			.SetGenesis("0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4adae5494dffff7f20020000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000");
 
 			return builder;
 		}
